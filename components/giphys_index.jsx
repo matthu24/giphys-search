00fb0d6bbf17b3b151bf12f30ae9ccd12750014c
Matthew Hu <matthu24@gmail.com>
2018-01-31 20:19:07 -0800

    Add example giphy api call data

--- components/giphys_index.jsx
+++ components/giphys_index.jsx
@@ -12,3 +12,38 @@ export default (props) => {
     </ul>
   )
 }
+
+
+
+//what the api call to giphy responds with:
+
+// {
+//     "data": [
+//         {
+//             type: "gif",
+//             id: "FiGiRei2ICzzG",
+//             slug: "funny-cat-FiGiRei2ICzzG",
+//             url: "http://giphy.com/gifs/funny-cat-FiGiRei2ICzzG",
+//             bitly_gif_url: "http://gph.is/1fIdLOl",
+//             bitly_url: "http://gph.is/1fIdLOl",
+//             embed_url: "http://giphy.com/embed/FiGiRei2ICzzG",
+//             username: "",
+//             source: "http://tumblr.com",
+//             rating: "g",
+//             caption: "",
+//             content_url: "",
+//             source_tld: "tumblr.com",
+//             source_post_url: "http://tumblr.com",
+//             import_datetime: "2014-01-18 09:14:20",
+//             trending_datetime: "1970-01-01 00:00:00",
+//             images: {
+//                 fixed_height: {
+//                     url: "http://media2.giphy.com/media/FiGiRei2ICzzG/200.gif",
+//                     width: "568",
+//                     height: "200",
+//                     size: "460622",
+//                     mp4: "http://media2.giphy.com/media/FiGiRei2ICzzG/200.mp4",
+//                     mp4_size: "13866",
+//                     webp: "http://media2.giphy.com/media/FiGiRei2ICzzG/200.webp",
+//                     webp_size: "367786"
+//                 },
